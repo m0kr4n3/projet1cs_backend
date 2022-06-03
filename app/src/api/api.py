b39d224cf9605b5161c24bc199680fa5d62161a7
m0kr4n3 <jm_abdelmalek@esi.dz>
2022-06-03 08:47:04 -0400

    adding static files

--- app/src/api/api.py
+++ app/src/api/api.py
@@ -2,10 +2,13 @@ from src.api.endpoints import login, users, items, lieux,categories, themes
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from src.core.config import settings
+from fastapi.staticfiles import StaticFiles
 
-api_router = FastAPI()
+api = FastAPI()
 
-@api_router.get("/")
+api.mount("/static", StaticFiles(directory="static"), name="static")
+
+@api.get("/")
 def read_root():
     return {"message": "Welcome to Projet 1CS backend"}
 
@@ -13,16 +16,16 @@ def read_root():
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
-    api_router.add_middleware(
+    api.add_middleware(
         CORSMiddleware,
         allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
-api_router.include_router(login.router, tags=["login"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(items.router, prefix="/items", tags=["items"])
-api_router.include_router(lieux.router, prefix="/lieux", tags=["lieux"])
-api_router.include_router(categories.router, prefix="/categories", tags=["categories"])
-api_router.include_router(themes.router, prefix="/themes", tags=["themes"])
+api.include_router(login.router, tags=["login"])
+api.include_router(users.router, prefix="/users", tags=["users"])
+api.include_router(items.router, prefix="/items", tags=["items"])
+api.include_router(lieux.router, prefix="/lieux", tags=["lieux"])
+api.include_router(categories.router, prefix="/categories", tags=["categories"])
+api.include_router(themes.router, prefix="/themes", tags=["themes"])
